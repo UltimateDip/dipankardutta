8b23aaf5fb731434c5b8522a92de3391f83f2f4e
Dipankar <dipankardutt1234567@gmail.com>
2024-08-04 23:56:21 +0530

    Fix failing test

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,12 @@
-import { render, screen } from '@testing-library/react';
-import App from './App';
+import { render, screen } from "@testing-library/react";
+import App from "./App";
 
-test('renders learn react link', () => {
+// write a test to check if the App component renders a number that changes every second
+test("renders a number that changes every second", () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+  const numberElement = screen.getByText(/Alive \d+/);
+  const number = Number(numberElement.textContent);
+  setTimeout(() => {
+    expect(Number(numberElement.textContent)).toBeGreaterThan(number);
+  }, 1000);
 });
